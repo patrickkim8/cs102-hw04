b223653adb062494f618c2b6ae2284f7e7759c82
patrickkim8 <55516266+patrickkim8@users.noreply.github.com>
2019-12-18 16:35:27 -0500

    Submitting the Image Filter Code

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import math
 from PIL import Image
 
 assert len(sys.argv) == 3, "Please specify an input path and output path"
@@ -15,23 +16,44 @@ new_img = Image.new("RGB", (width, height), "white")
 
 # TODO: Replace this with your own filter!
 # Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [0] * 9
-for i in range(2, width // 2):
-    for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        new_img.putpixel((i, j), (random.choice(members)))
+for i in range(1, width-1):
+    for j in range(1, height-1):
 
-for i in range(width // 2, width - 1):
-    for j in range(1, height - 1):
+        Fx = 0
+        Fy = 0
+
+        a = img.getpixel((i-1, j-1))
+        r = a[0]
+        g = a[1]
+        b = a[2]
+
+        intensity = r + g + b
+
+        Fx += -intensity
+        Fy += -intensity
+
+        a = img.getpixel((i-1, j))
+        r = a[0]
+        g = a[1]
+        b = a[2]
+
+        Fx += -2 * (r + g + b)
+
+        a = img.getpixel((i-1, j+1))
+        r = a[0]
+        g = a[1]
+        b = a[2]
+
+        Fx += -(r + g + b)
+        Fy += -(r + g + b)
+
+
+
+for i in range(width // 2, width):
+    for j in range(1, height):
         r, g, b = img.getpixel((i, j))
         new_img.putpixel((i, j), (0, g, b))
+        #find a function that gives you a number from 0 to 255
+        #rand something input to r, g, b
 
 new_img.save(output_path)
